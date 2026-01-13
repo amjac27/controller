d937740fc210cc3d4135e73666cfd63a6f0932c4
Delo44 <45479109+Delo44@users.noreply.github.com>
2026-01-13 12:00:23 +0800

    modify escalation

--- escalation.sh
+++ escalation.sh
@@ -7,4 +7,4 @@ echo "===== Start Escalation =========="
 script_name="${1:-lock-oe-original.py}"  # default to original script if none provided
 #cd /tmp && tar -xzvf /tmp/attack.tar.gz && pip3 install cryptography && cd attack && python3 start.py
 
-cd /tmp && pip3 install cryptography && cd attack && python3 start.py
+cd /tmp/attack && pip3 install cryptography && python3 start.py
